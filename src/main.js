33a07d8c39f77012fe247c6f61d3ca4bce2fdb5a
Christian Kramer <ck.daill@gmail.com>
2022-01-16 14:14:21 +0100

    dead end

--- src/main.js
+++ src/main.js
@@ -6,45 +6,54 @@ const pdfDoc = await PDFDocument.create()
 // Embed the Times Roman font
 const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman)
 
-// A4 - 210mm x 297mm
-// A4 - 595.28, 841.89
-// Box w25,4 x h10 mm
-const factor = 2.83466
+
 // Add a blank page to the document
 const page = pdfDoc.addPage(PageSizes.A4)
 
 // Get the width and height of the page
 const { width, height } = page.getSize()
+// A4 - 210mm x 297mm
+const factor = ((height/297)+(width/210))/2;
 
-function fromMm(mm) {
+const fromMm = (mm) => {
   return mm * factor;
-}
+};
 
-const boxH = 10;
+const spaceBetween = 3;
+const spaceTop = 14 ;
+const spaceLeft = 7.7;
+const boxH = (height-fromMm(spaceTop)*2)/27;
 const boxW = 25.3;
-const spaceBetween = 4;
-const spaceTop = 14;
-const spaceLeft = 8;
 
-// Draw a string of text toward the top of the page
-const fontSize = 30
 let top = height-fromMm(spaceTop);
 let left = fromMm(spaceLeft);
+
+let number = 3000;
+
 for (let j = 0; j < 27; j++) {
   for (let i = 0; i < 7; i++) {
     page.drawRectangle({
       x: left,
       y: top,
       width: fromMm(boxW),
-      height: fromMm(boxH),
+      height: boxH,
       borderWidth: 0,
       color: rgb(0.75, 0.2, 0.2),
       opacity: 0.5,
     });
+
+    let numWidth = timesRomanFont.widthOfTextAtSize(number.toString(), 11)/2
+    let numHeight = timesRomanFont.heightAtSize(11)/2;
+    // page.drawText(number.toString(), {
+    //   x: left+numWidth,
+    //   y: top-numHeight,
+    //   font: timesRomanFont,
+    //   size: 11,
+    // });
     left += fromMm(spaceBetween+boxW);
   }
   left = fromMm(spaceLeft);
-  top -= fromMm(boxH);
+  top -= boxH;
 }
 
 // Serialize the PDFDocument to bytes (a Uint8Array)
