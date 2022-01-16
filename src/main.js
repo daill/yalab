fd44e67fb567fc41aadc7d64c28fd72a13edbba6
Christian Kramer <ck.daill@gmail.com>
2022-01-16 08:43:14 +0100

    sizes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { PDFDocument, StandardFonts, rgb } from 'pdf-lib'
+import { PDFDocument, StandardFonts, rgb, PageSizes } from 'pdf-lib'
 
 // Create a new PDFDocument
 const pdfDoc = await PDFDocument.create()
@@ -6,17 +6,24 @@ const pdfDoc = await PDFDocument.create()
 // Embed the Times Roman font
 const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman)
 
+// A4 - 210mm x 297mm
+// A4 - 595.28, 841.89
+const factor = 2.83466
 // Add a blank page to the document
-const page = pdfDoc.addPage()
+const page = pdfDoc.addPage(PageSizes.A4)
 
 // Get the width and height of the page
 const { width, height } = page.getSize()
 
+function fromMm(mm) {
+  return mm * factor;
+}
+
 // Draw a string of text toward the top of the page
 const fontSize = 30
 page.drawText('Creating PDFs in JavaScript is awesome!', {
-  x: 50,
-  y: height - 4 * fontSize,
+  x: fromMm(8),
+  y: height-fromMm(14),
   size: fontSize,
   font: timesRomanFont,
   color: rgb(0, 0.53, 0.71),
@@ -25,7 +32,7 @@ page.drawText('Creating PDFs in JavaScript is awesome!', {
 // Serialize the PDFDocument to bytes (a Uint8Array)
 const pdfBytes = await pdfDoc.save()
 
-function download(filename, text) {
+function download(filename, data, type) {
     var element = document.createElement('a');
     element.setAttribute('href', window.URL.createObjectURL(new Blob([data], {type: type})));
     element.setAttribute('download', filename);
@@ -34,20 +41,9 @@ function download(filename, text) {
     document.body.appendChild(element);
   
     element.click();
-  
+    window.URL.revokeObjectURL(url);
     document.body.removeChild(element);
   }
-  
 
-  if (data !== null && navigator.msSaveBlob)
-  return navigator.msSaveBlob(new Blob([data], { type: type }), name);
-var a = $("<a style='display: none;'/>");
-var url = window.URL.createObjectURL(new Blob([data], {type: type}));
-a.attr("href", url);
-a.attr("download", name);
-$("body").append(a);
-a[0].click();
-window.URL.revokeObjectURL(url);
-a.remove();
 
 download("pdf-lib_form_flattening_example.pdf", pdfBytes, "application/pdf");
