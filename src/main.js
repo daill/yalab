5d3e85c11a52f771251a077e0c7da3fbb76bbdec
Christian Kramer <daill@detx.fritz.box>
2022-01-17 15:33:58 +0100

    switched to jspdf as the positioning of all the other was just bollocks

--- src/main.js
+++ src/main.js
@@ -1,131 +1,43 @@
-import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
+import { jsPDF } from "jspdf";
 
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
-pdfMake.fonts = {
-  'Roboto': {
-    normal: 'Roboto-Regular.ttf',
-    bold: 'Roboto-Medium.ttf',
-    italics: 'Roboto-Italic.ttf',
-    bolditalics: 'Roboto-Italic.ttf'
-  }
-};
+// Default export is a4 paper, portrait, using millimeters for units
+const doc = new jsPDF({format: 'a4'});
 
 
-// width=595.28, height=841.89
-// width=210mm, height=297mm
-const factor = ((595.28/210)+(841.89/297))/2
-const toScale = (mm) => {
-  return mm*factor;
-}
+const boxH = 10;
+const boxW = 24.6;
+const space = 3.7;
 
-const boxH = toScale(10);
-const boxW = toScale(24);
-const spacer = toScale(3);
-const [left, top] = [toScale(8), toScale(13.5)]
+const topMargin = 14 + doc.getTextDimensions('text').h;
+const leftMargin = 8;
 
-let content = [
-  {
-    style: 'tableExample',
-    table: {
-      widths: [boxW, spacer, boxW, spacer, boxW, spacer, boxW, spacer, boxW, spacer, boxW, spacer, boxW],
-      body: [
-        [
-          {
-            border: [false, false, false, false],
-            fillColor: '#dddddd',
-            text: '1'
-          },
-          {
-            border: [false, false, false, false],
-            fillColor: '#dddddd',
-            text: ''
-          },
-          {
-            border: [false, false, false, false],
-            fillColor: '#dddddd',
-            text: '2'
-          },
-          {
-            border: [false, false, false, false],
-            fillColor: '#dddddd',
-            text: ''
-          },
-          {
-            border: [false, false, false, false],
-            fillColor: '#dddddd',
-            text: '3'
-          },
-          {
-            border: [false, false, false, false],
-            fillColor: '#dddddd',
-            text: ''
-          },
-          {
-            border: [false, false, false, false],
-            fillColor: '#dddddd',
-            text: '4'
-          },
-          {
-            border: [false, false, false, false],
-            fillColor: '#dddddd',
-            text: ''
-          },
-          {
-            border: [false, false, false, false],
-            fillColor: '#dddddd',
-            text: '6'
-          },
-          {
-            border: [false, false, false, false],
-            fillColor: '#dddddd',
-            text: ''
-          },
-          {
-            border: [false, false, false, false],
-            fillColor: '#dddddd',
-            text: '7'
-          },
-        ],
-      ]
-    },
-    layout: {
-      defaultBorder: false,
+let top = topMargin;
+let left = leftMargin; 
+let dim = ''
+let count = 2016;
+
+//doc.setTextColor('#0000FF');
+
+let page = 0;
+for (let p = 0; p < 9; p++){
+  for (let i = 0; i < 27; i++) {
+    for (let j = 0; j < 7; j++) {
+      dim = doc.getTextDimensions(count.toString());
+      doc.text(count.toString(),left+(boxW/2)-(dim.w/2),top+(boxH/2)-(dim.h/2));    
+      left += space + boxW
+      count+=1;
     }
-  },
-];
-
-let currentHeight = top;
-let currentWidth = left;
-
-for (let j = 0; j < 27; j++) {
-  for (let i = 0; i < 7; i++) {
-    //content.push(element(currentWidth, currentHeight, "hallo"));
-    currentWidth += boxW + toScale(3);
+    left = leftMargin
+    top += boxH
   }
-  currentWidth = left;
-  currentHeight += boxH;
+  if (p != 8) {
+    doc.addPage()
+    top = topMargin;
+    left = leftMargin; 
+  }
 }
 
-const docDefinition = {
-  pageSize: 'A4',
-  content: content,
-  styles: {
-		tableExample: {
-			margin: [0, 0, 0, 0]
-		},
-  },
-  pageMargins: [left, top, left, top]
-}
-
-function element(x,y, text) {
-  return {
-    text: text,
-    absolutePosition: {x, y},
-  };
-};
-
-
+doc.save("etiketten.pdf");
 
 console.log(docDefinition);
 pdfMake.createPdf(docDefinition).download(`etiketten.pdf`);
